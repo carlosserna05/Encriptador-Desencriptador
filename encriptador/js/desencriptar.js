41acd062f51e3156e5fd6b76143eed3b87afd1f6
Carlos Serna <carlos.seven05@gmail.com>
2023-01-02 14:21:37 -0500

    first commit

---
+++ encriptador/js/desencriptar.js
@@ -0,0 +1,36 @@
+
+var botonDos = document.querySelector("#btn2");
+var mensaje = document.querySelector("#introducir-texto").value
+
+
+
+function desencriptar(){
+    document.getElementById("figura").style.display = "none";
+    var msjDesencriptado = "";
+    var mensaje = document.querySelector("#introducir-texto").value;
+    var mayus = /[A-Z]/g;
+    var caracteres = /[~áéíóúàèìòùÁÉÍÓÚ']/g;
+
+    if(mensaje == ""){
+        resultado(no)
+        document.getElementById("figura").style.display = "";
+    }
+    else if (mensaje.match(mayus)!= mensaje.match(mayus)){
+        resultado(no)
+        document.getElementById("figura").style.display = "";
+    }
+    else if (mensaje.match(caracteres) != mensaje.match(caracteres)){
+        resultado(no);
+        document.getElementById("figura").style.display = "";
+    }
+    else{
+        msjDesencriptado = mensaje.replace(/ai/g, "a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u"); 
+        document.getElementById("resultado").innerHTML=msjDesencriptado;
+        resultado(si)
+        document.getElementById("introducir-texto").value = "";
+    }
+    
+}
+
+  botonDos.onclick = desencriptar;
+  
